93517bd5e750a84fbc22b73d9dc3359b2e5a4768
Jokima <gkojima94@gmail.com>
2023-01-27 16:13:29 -0300

    Trabalho PO

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import YASMIJ from '../YASMIJ.js/dist/yasmij';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  tipos = [
+    { value: 'minimize', label: 'Minimizar' },
+    { value: 'maximize', label: 'Maximizar' },
+  ];
+  tipoSelecionado = 'minimize';
+  funcaoObjetivo = '';
+  restricoes: { valor: string }[] = [{ valor: '' }];
+
+  resultado: string = '';
+
+  ngOnInit(): void {}
+
+  adicionarRestricao() {
+    this.restricoes = [...this.restricoes, { valor: '' }];
+  }
+
+  removerRestricao() {
+    const newRestricoes = [...this.restricoes];
+    newRestricoes.splice(newRestricoes.length - 1, 1);
+    this.restricoes = newRestricoes;
+  }
+
+  resolverSimplex() {
+    const input = {
+      type: this.tipoSelecionado,
+      objective: this.funcaoObjetivo,
+      constraints: this.restricoes.map((rest) => rest.valor),
+    };
+
+    const output = YASMIJ.solve(input);
+    this.resultado = JSON.stringify(output)
+      .replaceAll(`"`, '')
+      .replaceAll('{', '')
+      .replaceAll('}', '')
+      .replace('result:', '')
+      .replaceAll(',', ', ')
+      .replaceAll('slack', 'folga')
+      .replaceAll(':', '=');
+  }
+
+  limpar() {
+    this.funcaoObjetivo = '';
+    this.restricoes = [{ valor: '' }];
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
